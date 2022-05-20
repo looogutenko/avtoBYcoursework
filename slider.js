6e2b54b938529d679547678b3c5b75f1ab19b6e4
ivan <looogutenko@gmail.com>
2022-05-21 02:26:38 +0300

    end

--- slider.js
+++ slider.js
@@ -1,88 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".carousel-container").forEach((carousel) => {
-      insertNumbers(carousel);
-  
-      carousel.querySelector(".prev").addEventListener("click", (e) => {
-        minusItem(carousel);
-      });
-  
-      carousel.querySelector(".next").addEventListener("click", () => {
-        plusItem(carousel);
-      });
-  
-      insertDots(carousel);
-  
-      carousel.querySelectorAll(".dot").forEach((dot) => {
-        dot.addEventListener("click", (e) => {
-          let item = Array.prototype.indexOf.call(
-            e.target.parentNode.children,
-            e.target
-          );
-  
-          showItems(carousel, item);
-        });
-      });
-  
-      showItems(carousel, 0);
-    });
-  });
-  
-  function insertNumbers(carousel) {
-    const length = carousel.querySelectorAll(".item").length;
-    for (let i = 0; i < length; i++) {
-      const nmbr = document.createElement("div");
-      nmbr.classList.add("numbertext");
-      nmbr.innerText = i + 1 + " / " + length;
-  
-      carousel.querySelectorAll(".item")[i].append(nmbr);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
     }
-  }
-  
-  function insertDots(carousel) {
-    const dots = document.createElement("div");
-    dots.classList.add("dots");
-  
-    carousel.append(dots);
-  
-    carousel.querySelectorAll(".item").forEach((elem) => {
-      const dot = document.createElement("div");
-      dot.classList.add("dot");
-  
-      carousel.querySelector(".dots").append(dot);
-    });
-  }
-  
-  function plusItem(carousel) {
-    let item = currentItem(carousel);
-  
-    carousel
-      .querySelectorAll(".item")
-      [item].nextElementSibling.classList.contains("item")
-      ? showItems(carousel, item + 1)
-      : showItems(carousel, 0);
-  }
-  
-  function minusItem(carousel) {
-    let item = currentItem(carousel);
-  
-    carousel.querySelectorAll(".item")[item].previousElementSibling != null
-      ? showItems(carousel, item - 1)
-      : showItems(carousel, carousel.querySelectorAll(".item").length - 1);
-  }
-  
-  function currentItem(carousel) {
-    return [...carousel.querySelectorAll(".item")].findIndex(
-      (item) => item.style.display == "block"
-    );
-  }
-  
-  function showItems(carousel, item) {
-    if (carousel.querySelectorAll(".item")[currentItem(carousel)] != undefined)
-      carousel.querySelectorAll(".item")[currentItem(carousel)].style.display =
-        "none";
-    carousel.querySelectorAll(".item")[item].style.display = "block";
-  
-    if (carousel.querySelector(".dot.active") != null)
-      carousel.querySelector(".dot.active").classList.remove("active");
-    carousel.querySelectorAll(".dot")[item].classList.add("active");
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
